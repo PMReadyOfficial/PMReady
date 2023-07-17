5fdfa3367fc10334db48f43065b45a5a9d964015
lem0n4id <leninkennedy4work@gmail.com>
2023-07-17 15:49:26 +0530

    feat: stats component - desktop high fidelity

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,20 +2,19 @@ export default function Stats() {
     return (
         // content: a text "State of the Industry" centered
         // content: 3 numbers with a text beside each number. example: 1,000+ Customers
-        <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl text-center">State of the Industry</h1>
-            <div className="flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-20 my-10">
-                <div className="flex flex-col lg:flex-row items-center justify-center gap-5">
-                    <h1 className="text-6xl">00</h1>
-                    <p className="text-2xl">lorem ipsum</p>
+        <div className="flex flex-col items-center justify-center bg-[#E8F5D8] text-[#023849]">
+            <div className="flex flex-col lg:flex-row items-center justify-center gap-5 my-24">
+                <div className="flex flex-col items-center justify-center gap-2 w-60">
+                    <h1 className="text-4xl font-lexend font-bold">$108,742</h1>
+                    <p className="text-lg text-center px-2 font-inter font-normal">PM Average Annual Salary in 2023</p>
                 </div>
-                <div className="flex flex-col lg:flex-row items-center justify-center gap-5">
-                    <h1 className="text-6xl">000</h1>
-                    <p className="text-2xl">lorem ipsum</p>
+                <div className="flex flex-col items-center justify-center gap-2 w-60 mx-12">
+                    <h1 className="text-4xl font-lexend font-bold">43.6%</h1>
+                    <p className="text-lg text-center font-inter font-normal">of companies were planning on hiring more PMs in 2022</p>
                 </div>
-                <div className="flex flex-col lg:flex-row items-center justify-center gap-5">
-                    <h1 className="text-6xl">000</h1>
-                    <p className="text-2xl">lorem ipsum</p>
+                <div className="flex flex-col items-center justify-center gap-2 w-60 mx-12">
+                    <h1 className="text-4xl font-lexend font-bold">3rd</h1>
+                    <p className="text-lg text-center font-inter font-normal">best job in the US according to Glassdoor</p>
                 </div>
             </div>
         </div>

43312daf22df25b28982995600a7e3c611af989f
lem0n4id <leninkennedy4work@gmail.com>
2023-08-06 09:27:31 +0530

    feat: Header Hifi v2
    Desktop version of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,33 @@ const Header = () => {
 
     return (
         <header>
-            <div className="flex py-4 px-2 lg:px-28 bg-[#F2F2F2]">
+            <div className="flex py-3 px-4 lg:px-28 bg-[#FEFDFF]">
                 <div id="logo" className="flex item-center pt-1">
                     <img src="/PMReady/Logo.svg" alt="" />
                 </div>
-                <div id="menu" className="flex flex-grow justify-end z-20">
-                    <ul className="flex space-x-4 items-center text-xl ">
+                <div id="menu" className="flex flex-grow justify-end z-20 font-inter text-[#24223C]">
+                    <ul className="flex gap-8 items-center text-lg">
                         <li className="hidden lg:block p-3 ">
-                            <a href="#" className="font-semibold hover:underline decoration-2">Courses</a>
+                            <a href="#" className="hover:underline decoration-2">Courses</a>
                         </li>
                         <li className="hidden lg:block p-3">
-                            <a href="#" className="font-semibold hover:underline decoration-2">Resources</a>
+                            <a href="#" className="hover:underline decoration-2">Resources</a>
                         </li>
                         <li className="hidden lg:block p-3">
-                            <a href="#" className="font-semibold hover:underline decoration-2">Job Board</a>
+                            <a href="#" className="hover:underline decoration-2">Job Board</a>
                         </li>
                         <li className="hidden lg:block p-3">
-                            <a href="#" className="font-semibold hover:underline decoration-2">About</a>
+                            <a href="#" className="hover:underline decoration-2">About</a>
                         </li>
+                        <li className="hidden lg:block p-2 rounded-full bg-[#E2E1EC]">
+                            <img src="/PMReady/user.svg" alt="" />                        </li>
                     </ul>
-                </div>
-                <div className="hidden lg:block pl-4 my-auto">
-                    <img src="/PMReady/user.svg" alt="" />
                 </div>
                 <div className="flex items-center ">
                     <a href="#">
                         <img className="block lg:hidden" src="/PMReady/menu.png" alt="" />
                     </a>
-                    <div className="hidden lg:flex lg:space-x-3 items-center"></div>
                 </div>
-                
-                <div id="user"></div>
             </div>
         </header>
     )

240cc07fb8356849a76d10d4f38fad3ee828bf14
Arnav Chaudhry <50321710+WebDev679@users.noreply.github.com>
2023-11-15 03:17:47 -0500

    feat: who are we component - home page (#26)
    * Update CONTRIBUTING.md
    
    * Added new component whoweare
    
    * fix: image path
    
    * fix: svg path
    
    * fix: center the component horizontally
    
    ---------
    
    Co-authored-by: Lenin Kennedy <leninkennedy4work@gmail.com>

---
+++ src/components/WhoWeAre.jsx
@@ -0,0 +1,21 @@
+export default function WhoWeAre() {
+    return (
+        <div style={{ backgroundImage: "url(vector1.svg), url(vector4.svg)", backgroundPosition: "left top, right 100%"}} className="h-[200px] items-center justify-center bg-no-repeat bg-[12%,13%] pr-[209px] pl-[209px] pt-[209px] pb-[409px]">
+            <div className="flex flex-row gap-5 w-full h-[300px] justify-center">
+                <div className="w-2/5 h-full bg-no-repeat bg-cover shrink-0">
+                    <img src="Rectangle.png" alt="" className="rounded-[0_0_80px_0]"/>
+                </div>
+                <div className="text-start w-2/5 pt-10">
+                    <h5 className="text-[color:var(--color-teal-500,#023849)] text-[32px] not-italic font-bold leading-9">Who we are</h5>
+                    <p className="text-[color:var(--color-dark-600,#24223C)] text-lg not-italic font-normal leading-7 my-5">
+                        Learn the secrets to product strategy, agile development, 
+                        and market research through our self-paced courses! 
+                        Learn the secrets to product strategy, agile development, 
+                        and market research through our self-paced courses!
+                    </p>
+                    <a className="@apply text-[color:var(--color-dark-600,#24223C)] text-center text-lg not-italic font-semibold underline pb-[30px]">Read our story</a>
+                </div>
+            </div>
+        </div>
+    )
+}  

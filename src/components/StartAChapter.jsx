e98a36f383bde7910fee4d0e22c2fa6af2c0fc9b
samantha <samanthap8675@gmail.com>
2023-11-14 21:01:46 -0500

    start a chapter

---
+++ src/components/StartAChapter.jsx
@@ -0,0 +1,22 @@
+
+export default function StartAChapter() {
+
+
+    return (
+       <div className="flex ">
+            <div className="relative ml-[110px] ">
+                <div className="mt-[132px] absolute w-[591px] h-[313.5px] bg-[#E8F5D8] rounded-[30px] ml-[134px]"></div>
+                <div className="ml-[255px] absolute w-[267px] h-[217.5px] bg-[#d9d9d9]"></div>
+                <div className="ml-[552px] absolute w-[267px] h-[217.5px] bg-[#d9d9d9]"></div>
+                <div className="mt-[180px] ml-[354px] absolute w-[267px] h-[217.5px] bg-[#aeaeae]"></div>
+                <div className="mt-[180px] ml-[651px] absolute w-[267px] h-[217.5px] bg-[#aeaeae]"></div>
+            </div>
+            <div className="w-[765px] ml-[975px] mt-[82.5px]">
+                <div className="text-[54px] font-bold">Start a Chapter</div>
+                <div className="mt-[24px] ">Ask questions,share, and network on our dedicated Discord server</div>
+                <a className="font-semibold text-[20px] mt-[27px] underline">Join Discord</a>
+                <img className="inline ml-[12px] w-[36px] h-[36px]" src="Chevron_Right.svg"></img>
+            </div>
+       </div>
+    ) 
+}

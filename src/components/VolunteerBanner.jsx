6458cfe1f123fc534ff4e5534f56ae5fd1f7959b
samanthap88 <124219829+samanthap88@users.noreply.github.com>
2023-11-15 03:02:56 -0500

    feat: volunteer banner component - about page (#19)
    * added volunteer banner
    
    * changed styling
    
    * fix: Improved styling using <span> for inline text
    
    Replaced <p> tag with <span> for inline styling in accordance with best practices
    
    ---------
    
    Co-authored-by: Lenin Kennedy <leninkennedy4work@gmail.com>

---
+++ src/components/VolunteerBanner.jsx
@@ -0,0 +1,38 @@
+
+export default function VolunteerBanner() {
+    return (
+        <div className="text-center justify-center" >
+            <h1  className="font-lexend text-4xl font-bold mt-10 " >
+                PMReady is made <span className="text-[#FA9D74]">by PMs, </span> <span className="text-[#3BCABE]">for PMs</span>
+            </h1>
+            <div className="grid mt-10 px-16 grid-cols-1 md:grid-cols-4 gap-5 w-[1040px] mx-auto">
+                <div className="inline-block justify-center text-center">
+                    <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
+                    <p className="font-lexend">Director of Awesome</p>
+                </div>
+                <div className="grid-rows-2 gap-2 md:grid-rows-1">
+                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full" ></img>
+                    <p  className="font-lexend text-lg font-semibold mt-3">John Doe Smith</p>
+                    <p className="font-lexend">Director of Awesome</p>
+                </div>
+                <div className="grid-rows-2 gap-2 md:grid-rows-1">
+                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
+                    <p className="font-lexend">Director of Awesome</p>
+                </div>
+                <div className="grid-rows-2 gap-2 md:grid-rows-1">
+                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
+                    <p className="font-lexend" >Director of Awesome</p>
+                </div>
+            </div>
+
+            <h2 className="font-lexend font-bold text-2xl mt-10">We <p className="text-[#FA9D74] inline ">love</p> our interns and volunteers!</h2>
+            <img src = "image9.png" className="mt-5 mx-auto w-[864px] h-[542px]"></img>
+        </div>
+        
+
+       
+    ) 
+}

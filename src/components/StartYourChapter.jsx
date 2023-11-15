5f69a900fe9d412b7178fb8feda365b04c59890f
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-11-16 00:19:10 +0530

    fix(minor): header and footer (#29)
    * fix: links in header component
    
    * fix: bottom spacing in footer

--- src/components/StartYourChapter.jsx
+++ src/components/StartYourChapter.jsx
@@ -1,6 +1,6 @@
 export default function StartYourChapter() {
     return (
-        <section className=" bg-[#24223C] px-8 mx0 sm:px-8 md:px-16 pt-20 pb-20">
+        <section className=" bg-[#24223C] px-8 mx0 sm:px-8 md:px-16 pt-20 pb-5">
             <div className="text-center">
                 <h1 className="text-white font-bold text-3xl pb-8">Start your chapter at university</h1>
                 <div className='px-10'>
@@ -11,7 +11,7 @@ export default function StartYourChapter() {
             </div>
 
 
-            <div class="grid grid-cols-6 my-20">
+            <div class="grid grid-cols-6 mt-20 my-10">
                 <div className="mx-5">
                     <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
                 </div>

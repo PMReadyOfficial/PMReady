811e2c0ed057d310c900232ca5aa4c598ba3dae7
Jason Jiang <69778744+jasonjiang9142@users.noreply.github.com>
2023-11-15 03:05:40 -0500

    feat: start your chapter component - home page (#22)
    Co-authored-by: Lenin Kennedy <leninkennedy4work@gmail.com>

---
+++ src/components/StartYourChapter.jsx
@@ -0,0 +1,39 @@
+export default function StartYourChapter() {
+    return (
+        <section className=" bg-[#24223C] px-8 mx0 sm:px-8 md:px-16 pt-20 pb-20">
+            <div className="text-center">
+                <h1 className="text-white font-bold text-3xl pb-8">Start your chapter at university</h1>
+                <div className='px-10'>
+                    <p className="text-white font-bold pb-8 px-28 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsum assumenda, fuga dolorum ut hic doloremque exercitationem itaque perferendis numquam similique blanditiis illum vel molestiae quaerat suscipit recusandae ab enim.</p>
+                </div>
+
+                <button className="bg-[#FA9D74] rounded-full font-bold py-4 px-6">Join our Chapters</button>
+            </div>
+
+
+            <div class="grid grid-cols-6 my-20">
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+                <div className="mx-5">
+                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                </div>
+            </div>
+
+
+
+        </section>
+    )
+}

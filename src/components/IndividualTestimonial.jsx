0fa462e70071c831dc79cf82a8540debd631463a
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-12-04 15:19:33 +0530

    feat: add carousel effect

--- src/components/IndividualTestimonial.jsx
+++ src/components/IndividualTestimonial.jsx
@@ -1,9 +1,9 @@
 export default function IndividualTestimonial({ content = 'lorem' }) {
     return (
-        <div style={{ backgroundColor: '#E8F5D8' }} className="p-10 rounded-3xl w-1/3">
+        <div style={{ backgroundColor: '#E8F5D8' }} className="p-10 rounded-3xl w-full select-none">
             <div className="flex mb-10 justify-center">
                 <p className="font-bold text-lg">“</p>
-                <h1 className="font-bold px-10 text-lg ">
+                <h1 className="font-bold px-10 text-lg text-center">
                     {content}
                 </h1>
                 <p className="font-bold text-lg">”</p>
--- src/components/Testimonal.jsx
+++ src/components/Testimonal.jsx
@@ -1,27 +1,48 @@
 import IndividualTestimonial from "./IndividualTestimonial";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export default function Testimonial() {
     const testimonials = [
-        { id: 1, content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ' },
-        { id: 2, content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ' },
-        { id: 3, content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. ' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
+
     ]
 
     return (
-        <section className='my-20 mx-10'>
-            <div className="flex justify-between space-x-8">
-                {/* Assuming you're mapping through an array of testimonials */}
-                {testimonials.map((testimonial, index) => (
-                    <IndividualTestimonial key={index} content={testimonial.content} className="" />
+        <div className="mx-">
+            <Swiper
+                style={{
+                    "--swiper-pagination-color": "#9ADB67",
+                    "--swiper-pagination-bullet-inactive-color": "#265019",
+                    "--swiper-pagination-bullet-inactive-opacity": "1",
+                    "--swiper-pagination-bullet-size": "8px",
+                    "--swiper-pagination-bullet-horizontal-gap": "8px"
+                }}
+                initialSlide="1"
+                loop={true}
+                centeredSlides={true}
+                spaceBetween={50}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {testimonials.map((testimonial) => (
+                    <SwiperSlide key={testimonial.id}><IndividualTestimonial  content={testimonial.content} className="" /></SwiperSlide>
                 ))}
-            </div>
-
-
-            <div className="flex justify-center mt-4">
-                {testimonials.map((testimonial, index) => {
-                    return <span className="text-3xl">•</span>
-                })}
-            </div>
-        </section>
+            </Swiper>
+        </div>
     );
 }

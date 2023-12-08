b945dc3c11eb9c68ba7c2200498750deb60a4592
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-12-09 02:03:33 +0530

    fix: refactor WhoWeAre component styling

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -1,19 +1,21 @@
 export default function WhoWeAre() {
     return (
-        <div style={{ backgroundImage: "url(vector1.svg), url(vector4.svg)", backgroundPosition: "left top, right 100%"}} className=" items-center justify-center bg-no-repeat bg-[12%,13%] lg:pr-[209px] pr-[20px] lg:pl-[209px] pl-[20px] pt-[209px] lg:pb-[409px] pb-[164px] mb-16">
-            <div className="flex flex-col sm:flex-row gap-5 w-full h-[300px] justify-center">
-                <div className="w-full sm:w-2/5 h-full bg-no-repeat bg-cover">
+        <div className="relative items-center justify-center pr-[20px] lg:pr-[209px] pl-[20px] lg:pl-[209px] pt-[209px] lg:pb-52 pb-20">
+            <img src="Graphic_Leaf.png" alt="" className="absolute -top-[65px] sm:top-0 -left-0 scale-50 -translate-x-14 lg:translate-x-0 lg:scale-100 -z-10"/>
+            <img src="Graphic_Leaf_1.png" alt="" className="absolute -bottom-[100px] sm:bottom-0 right-0 scale-50 translate-x-14 lg:translate-x-0 lg:scale-100 -z-10"/>
+            <div className="flex flex-col lg:flex-row gap-5 w-full items-center lg:justify-center">
+                <div className="max-w-md h-full bg-no-repeat bg-cover">
                     <img src="Rectangle.png" alt="" className="w-full rounded-[0_0_80px_0]"/>
                 </div>
-                <div className="text-start w-full sm:w-2/5 pt-10">
-                    <h5 className="text-[color:var(--color-teal-500,#023849)]  text-[32px] sm:text-[24px] not-italic font-bold leading-9">Who we are</h5>
-                    <p className="text-[color:var(--color-dark-600,#24223C)] text-lg not-italic font-normal leading-7 my-5">
+                <div className="text-start w-full max-w-lg sm:w-2/5 pt-10">
+                    <h5 className="font-lexend text-[color:var(--color-teal-500,#023849)]  text-[32px] sm:text-[24px] not-italic font-bold leading-9">Who we are</h5>
+                    <p className="font-inter text-[color:var(--color-dark-600,#24223C)] text-lg not-italic font-normal leading-7 my-5">
                         Learn the secrets to product strategy, agile development, 
                         and market research through our self-paced courses! 
                         Learn the secrets to product strategy, agile development, 
                         and market research through our self-paced courses!
                     </p>
-                    <a className="@apply text-[color:var(--color-dark-600,#24223C)] text-center text-lg not-italic font-semibold underline pb-[30px]">Read our story</a>
+                    <a className="font-lexend @apply text-[color:var(--color-dark-600,#24223C)] text-center text-lg not-italic font-semibold underline pb-[30px]">Read our story</a>
                 </div>
             </div>
         </div>

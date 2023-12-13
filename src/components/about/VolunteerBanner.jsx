0733b942c7bcafb985c02cce45d2695256f4ab12
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-12-14 05:16:33 +0530

    feat: VolunteerBanner component - mobile

--- src/components/about/VolunteerBanner.jsx
+++ src/components/about/VolunteerBanner.jsx
@@ -2,11 +2,11 @@
 export default function VolunteerBanner() {
     return (
         <div className="text-center justify-center" >
-            <h1 className="font-lexend text-4xl font-bold mt-10 " >
-                PMReady is made <span className="text-[#FA9D74]">by PMs, </span> <span className="text-[#3BCABE]">for PMs</span>
+            <h1 className="font-lexend text-[32px] leading-9 lg:text-4xl font-bold mt-10 " >
+                PMReady is made <br className="block lg:hidden" /> <span className="text-[#FA9D74]">by PMs, </span> <span className="text-[#3BCABE]">for PMs</span>
             </h1>
             <div className="flex flex-col gap-5 items-center mt-[36px]">
-                <div className="flex flex-row gap-5">
+                <div className="flex flex-row gap-5 hidden lg:block">
                     <div className="inline-block justify-center text-center w-[211px]">
                         <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
@@ -23,7 +23,7 @@ export default function VolunteerBanner() {
                         <p className="font-inter text-lg mt-2">Product Manager</p>
                     </div>
                 </div>
-                <div className="flex flex-row gap-5">
+                <div className="flex flex-row gap-5 hidden lg:block">
                     <div className="inline-block justify-center text-center w-[211px]">
                         <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Lenin Kennedy</p>
@@ -40,13 +40,52 @@ export default function VolunteerBanner() {
                         <p className="font-inter text-lg mt-2">Designer</p>
                     </div>
                 </div>
+
+                <div className="flex flex-row gap-2 block lg:hidden">
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Co-Founder</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Johnny Huang</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Co-Founder</p>
+                    </div>
+                    
+                </div>
+                <div className="flex flex-row gap-2 block lg:hidden">
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Catherine Tai</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Product Manager</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Lenin Kennedy</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Lead Software Engineer</p>
+                    </div>
+                    
+                </div>
+                <div className="flex flex-row gap-2 block lg:hidden">
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Yasvi Patel</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Designer</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
+                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Jennifer Im</p>
+                        <p className="font-inter text-base min-[400px]:text-lg mt-2">Designer</p>
+                    </div>
+                </div>
                 
             </div>
 
             <h2 className="font-lexend font-bold text-2xl mt-10">
                 We <span className="text-[#FA9D74]">love</span> our interns and volunteers!
             </h2>
-            <img src = "image9.png" className="mt-5 mx-auto w-[864px] h-[542px] rounded-[40px]"></img>
+            <img src = "image9.png" className="mt-5 mx-auto max-w-[300px] min-[400px]:max-w-sm lg:max-w-[864px] rounded-[40px] object-scale-down"></img>
         </div>
         
 

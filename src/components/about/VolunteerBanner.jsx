32f6fcfbbf7f01cb1e02a245bb771a7a4f714239
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-12-14 04:51:56 +0530

    fix: refactor to use flex

--- src/components/about/VolunteerBanner.jsx
+++ src/components/about/VolunteerBanner.jsx
@@ -2,34 +2,51 @@
 export default function VolunteerBanner() {
     return (
         <div className="text-center justify-center" >
-            <h1  className="font-lexend text-4xl font-bold mt-10 " >
+            <h1 className="font-lexend text-4xl font-bold mt-10 " >
                 PMReady is made <span className="text-[#FA9D74]">by PMs, </span> <span className="text-[#3BCABE]">for PMs</span>
             </h1>
-            <div className="grid mt-10 px-16 grid-cols-1 md:grid-cols-4 gap-5 w-[1040px] mx-auto">
-                <div className="inline-block justify-center text-center">
-                    <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
-                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
-                    <p className="font-lexend">Director of Awesome</p>
+            <div className="flex flex-col gap-5 items-center mt-[36px]">
+                <div className="flex flex-row gap-5">
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
+                        <p className="font-inter text-lg mt-2">Co-Founder</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Johnny Huang</p>
+                        <p className="font-inter text-lg mt-2">Co-Founder</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Catherine Tai</p>
+                        <p className="font-inter text-lg mt-2">Product Manager</p>
+                    </div>
                 </div>
-                <div className="grid-rows-2 gap-2 md:grid-rows-1">
-                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full" ></img>
-                    <p  className="font-lexend text-lg font-semibold mt-3">John Doe Smith</p>
-                    <p className="font-lexend">Director of Awesome</p>
+                <div className="flex flex-row gap-5">
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Lenin Kennedy</p>
+                        <p className="font-inter text-lg mt-2">Lead Software Engineer</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Yasvi Patel</p>
+                        <p className="font-inter text-lg mt-2">Designer</p>
+                    </div>
+                    <div className="inline-block justify-center text-center w-[211px]">
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <p className="font-lexend text-lg font-semibold mt-2" >Jennifer Im</p>
+                        <p className="font-inter text-lg mt-2">Designer</p>
+                    </div>
                 </div>
-                <div className="grid-rows-2 gap-2 md:grid-rows-1">
-                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
-                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
-                    <p className="font-lexend">Director of Awesome</p>
-                </div>
-                <div className="grid-rows-2 gap-2 md:grid-rows-1">
-                    <img className="block mx-auto w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
-                    <p className="font-lexend text-lg font-semibold mt-3" >John Doe Smith</p>
-                    <p className="font-lexend" >Director of Awesome</p>
-                </div>
+                
             </div>
 
-            <h2 className="font-lexend font-bold text-2xl mt-10">We <p className="text-[#FA9D74] inline ">love</p> our interns and volunteers!</h2>
-            <img src = "image9.png" className="mt-5 mx-auto w-[864px] h-[542px]"></img>
+            <h2 className="font-lexend font-bold text-2xl mt-10">
+                We <span className="text-[#FA9D74]">love</span> our interns and volunteers!
+            </h2>
+            <img src = "image9.png" className="mt-5 mx-auto w-[864px] h-[542px] rounded-[40px]"></img>
         </div>
         
 

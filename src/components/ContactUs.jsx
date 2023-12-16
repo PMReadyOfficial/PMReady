e9d29d34632f3a6eb86f539e87fad4f0c433b0c2
samantha <samanthap8675@gmail.com>
2023-12-16 10:09:37 -0500

    contact us mobile

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,20 +1,20 @@
 export default function ContactUs() {
     return (
         <div className='bg-customgreen100 py-16 my-10'>
-            <div className="mx-40 ">
+            <div className="lg:mx-40 ">
                 <div>
-                    <h1 className="text-center font-bold text-3xl">Have additional <span className="text-orange-400">Questions?</span></h1>
+                    <h1 className="text-center font-bold text-3xl">Have additional <br className="lg:hidden"/><span className="text-orange-400">Questions?</span></h1>
                 </div>
 
-                <form action="" className="mx-20">
-                    <div className="grid grid-cols-2 my-9">
-                        <div className="pr-4">
+                <form action="" className="mx-4 lg:mx-20">
+                    <div className="grid lg:grid-cols-2 my-9">
+                        <div className="lg:pr-4">
                             <label htmlFor="name" className="text-sm text-gray-400">Name</label>
                             <br />
                             <input type="text" id="name" className="w-full border border-gray-400 p-3 rounded-md" placeholder="Name" />
                         </div>
 
-                        <div className="pl-4">
+                        <div className="my-4 lg:pl-4">
                             <label htmlFor="email" className="text-sm text-gray-400">Email</label>
                             <br />
                             <input type="email" id="email" className="w-full border border-gray-400 p-3 rounded-md" placeholder="Email@email.com" />
@@ -29,7 +29,7 @@ export default function ContactUs() {
                     </div>
 
                     <div className="flex justify-end my-4">
-                        <button className="py-3 px-7 rounded-full bg-customteal500">
+                        <button className="w-full lg:w-52 py-3 px-7 rounded-full bg-customteal500">
                             <span className="font-bold text-white">Send Message</span>
                         </button>
                     </div>

9ba88b26532d6731af0cb81d324dba6d404d437b
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-13 20:11:04 +0530

    fix: update images and text - About Page

--- src/components/about/VolunteerBanner.jsx
+++ src/components/about/VolunteerBanner.jsx
@@ -8,34 +8,34 @@ export default function VolunteerBanner() {
             <div className="flex flex-col gap-5 items-center mt-[36px]">
                 <div className="flex flex-row gap-5 hidden lg:block">
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 rounded-full" src="./images/macy.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
                         <p className="font-inter text-lg mt-2">Co-Founder</p>
                     </div>
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/johnny.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Johnny Huang</p>
                         <p className="font-inter text-lg mt-2">Co-Founder</p>
                     </div>
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/catherine.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Catherine Tai</p>
                         <p className="font-inter text-lg mt-2">Product Manager</p>
                     </div>
                 </div>
                 <div className="flex flex-row gap-5 hidden lg:block">
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/lenin.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Lenin Kennedy</p>
                         <p className="font-inter text-lg mt-2">Lead Software Engineer</p>
                     </div>
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/yasvi.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Yasvi Patel</p>
                         <p className="font-inter text-lg mt-2">Designer</p>
                     </div>
                     <div className="inline-block justify-center text-center w-[211px]">
-                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/jennifer.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Jennifer Im</p>
                         <p className="font-inter text-lg mt-2">Designer</p>
                     </div>
@@ -85,7 +85,7 @@ export default function VolunteerBanner() {
             <h2 className="font-lexend font-bold text-2xl mt-10">
                 We <span className="text-[#FA9D74]">love</span> our interns and volunteers!
             </h2>
-            <img src = "image9.png" className="mt-5 mx-auto max-w-[300px] min-[400px]:max-w-sm lg:max-w-[864px] rounded-[40px] object-scale-down"></img>
+            <img src = "./images/image-9.jpeg" className="mt-5 mx-auto max-w-[300px] min-[400px]:max-w-sm lg:max-w-[864px] rounded-[40px] object-scale-down"></img>
         </div>
         
 

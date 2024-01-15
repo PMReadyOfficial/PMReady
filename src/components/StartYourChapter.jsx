2a746fdab4061f14001faabe424a8b6905ebec1c
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-15 17:15:11 +0530

    fix: update text, images and link - Home Page

--- src/components/StartYourChapter.jsx
+++ src/components/StartYourChapter.jsx
@@ -1,34 +1,35 @@
 export default function StartYourChapter() {
     return (
         <section className=" bg-[#24223C] px-8 mx-0 sm:px-8 md:px-16 pt-20 pb-5">
-            <div className="text-center">
+            <div className="flex flex-col items-center text-center">
                 <h1 className="text-white font-bold text-3xl pb-8 sm:pb-5">Start your chapter at university</h1>
                 <div className='px-10 sm:px-8'>
-                    <p className="text-white font-bold pb-8 sm:pb-10 lg:px-28 sm:px-8 md:px-8  ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ipsum assumenda, fuga dolorum ut hic doloremque exercitationem itaque perferendis numquam similique blanditiis illum vel molestiae quaerat suscipit recusandae ab enim.</p>
+                    <p className="text-white font-bold pb-8 sm:pb-10 lg:px-28 sm:px-8 md:px-8 max-w-4xl">Want to start a new chapter at your university? Fill in the form right now, and we will get you on a call with someone on our team!</p>
                 </div>
-
-                <button className="bg-[#FA9D74] rounded-full font-bold py-4 px-6">Join our Chapters</button>
+                <a href="./community">
+                    <button className="bg-[#FA9D74] rounded-full font-bold py-4 px-6">Join our Chapters</button>
+                </a>
             </div>
 
 
             <div className="grid grid-cols-6 mt-20 my-10">
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="microsoft.png" alt="Microsoft logo" />
                 </div>
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="images/image-9.jpeg" alt="" />
                 </div>
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="tesla.png" alt="Tesla logo" />
                 </div>
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="images/image-15.jpeg" alt="" />
                 </div>
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="apple.png" alt="Apple logo" />
                 </div>
-                <div className="mx-2.5 lg:mx-5">
-                    <img src="https://media.tarkett-image.com/large/TH_24567080_24594080_24596080_24601080_24563080_24565080_24588080_001.jpg" alt="" />
+                <div className="mx-2.5 lg:mx-5 my-auto">
+                    <img src="images/image-16.jpeg" alt="" />
                 </div>
             </div>
 
--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -5,17 +5,14 @@ export default function WhoWeAre() {
             <img src="Graphic_Leaf_1.png" alt="" className="absolute -bottom-[100px] sm:bottom-0 right-0 scale-50 translate-x-14 lg:translate-x-0 lg:scale-100 -z-10"/>
             <div className="flex flex-col lg:flex-row gap-5 w-full items-center lg:justify-center">
                 <div className="max-w-md h-full bg-no-repeat bg-cover">
-                    <img src="Rectangle.png" alt="" className="w-full rounded-[0_0_80px_0]"/>
+                    <img src="whoweare.jpeg" alt="" className="w-full rounded-[0_0_80px_0]"/>
                 </div>
                 <div className="text-start w-full max-w-lg sm:w-2/5 pt-10">
                     <h5 className="font-lexend text-[color:var(--color-teal-500,#023849)]  text-[32px] sm:text-[24px] not-italic font-bold leading-9">Who we are</h5>
                     <p className="font-inter text-[color:var(--color-dark-600,#24223C)] text-lg not-italic font-normal leading-7 my-5">
-                        Learn the secrets to product strategy, agile development, 
-                        and market research through our self-paced courses! 
-                        Learn the secrets to product strategy, agile development, 
-                        and market research through our self-paced courses!
+                    At PMReady, we are more than just an organization; We are a community of passionate individuals dedicated to shaping the next generation of Product Managers, Program Managers and Project Managers. We believe that talent knows no boundaries, and every student deserves the opportunity to learn, grow, and break into the dynamic world of Product Management.
                     </p>
-                    <a className="font-lexend @apply text-[color:var(--color-dark-600,#24223C)] text-center text-lg not-italic font-semibold underline pb-[30px]">Read our story</a>
+                    <a href="./about" className="font-lexend @apply text-[color:var(--color-dark-600,#24223C)] text-center text-lg not-italic font-semibold underline pb-[30px]">Read our story</a>
                 </div>
             </div>
         </div>

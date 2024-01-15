8f7600ad067df4f8712f2cee5dfd3b8fc161ddb3
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-01-15 17:29:30 +0530

    fix: link the logo and change order of items - Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,15 @@ const Header = () => {
         <header>
             <div className="flex py-3 px-4 lg:px-28 bg-[#FEFDFF]">
                 <div id="logo" className="flex item-center pt-1">
-                    <img src="Logo.svg" alt="" />
+                    <a href="./">
+                        <img src="Logo.svg" alt="" />
+                    </a>
                 </div>
                 <div id="menu" className="flex flex-grow justify-end z-20 font-inter text-[#24223C]">
                     <ul className="flex gap-8 items-center text-lg">
+                        <li className="hidden lg:block p-3">
+                            <a href="./about" className="hover:underline decoration-2">About</a>
+                        </li>
                         <li className="hidden lg:block p-3 ">
                             <a href="./resources" className="hover:underline decoration-2">Resources</a>
                         </li>
@@ -17,11 +22,7 @@ const Header = () => {
                         <li className="hidden lg:block p-3">
                             <a href="./contact" className="hover:underline decoration-2">Contact</a>
                         </li>
-                        <li className="hidden lg:block p-3">
-                            <a href="./about" className="hover:underline decoration-2">About</a>
-                        </li>
-                        {/* <li className="hidden lg:block p-2 rounded-full bg-[#E2E1EC]">
-                            <img src="user.svg" alt="" />                        </li> */}
+                        
                     </ul>
                 </div>
                 <div className="flex items-center ">

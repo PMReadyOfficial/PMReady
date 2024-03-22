77d1987933986f96212a9abf20b64f7083599a11
samantha <samanthap8675@gmail.com>
2024-03-22 12:52:08 -0400

    static

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,9 +48,9 @@ export default function Hero() {
                     <div className="ml-3 min-[400px]:ml-10 lg:ml-32 mt-10 lg:mt-20 font-lexend">
                         <h3 className="text-lg lg:text-2xl font-bold text-[#9C9AB7]">We are a one-stop shop for</h3>
                         <h1 className="text-5xl lg:text-7xl font-bold text-[#24223C] pb-3">
-                            <span className="typewriter-program">Program</span> <br />
+                            <span >Program</span> <br />
 
-                            <span className="typewriter-management block text-[#FA9D74] pt-2">Management</span>
+                            <span className="block text-[#FA9D74] pt-2">Management</span>
                         </h1>
                         <div className="CTA flex gap-3 mt-10 lg:text-2xl font-semibold">
                             <button className="px-10 py-3 bg-[#023849] border-4 text-[#FEFDFF] border-[#023849] rounded-[100px]">Join us on Discord</button>

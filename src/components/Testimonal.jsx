b580db5bd88ba62ebd3ae923e9232937dfb0529d
DORUK SAVASAN <doruk@crc-dot1x-nat-10-239-243-67.bu.edu>
2024-03-29 14:57:47 -0400

    finished the testimonials

--- src/components/Testimonal.jsx
+++ src/components/Testimonal.jsx
@@ -8,13 +8,14 @@ import 'swiper/css/pagination';
 
 export default function Testimonial() {
     const testimonials = [
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-        { id: 1, content: 'Lorem ipsum dolor sit amet consectetur. Sed dis placerat tristique senectus pretium.' },
-
+        { id: 1, content: 'PMReady allowed me to get hands-on experience with Product Management. Although I’m studying computer science, software engineering is not my passion, PMReady helped me find a passion that I want to pursue.', author:"Colorful Unicorn, Computer Science 2026"},
+        { id: 2, content: 'When I joined PMReady, I found a role that not only would allow me to leverage my pre-existing skills, but encouraged me to use them in a new context to make a real difference in a company.', author: "Owen Mariani, Computer Science 2026"},
+        { id: 3, content: 'PMReady has reshaped my view of what goes into a product and pushes me to make ones that harness market research, design principles, and feature planning.', author: "Sleeping Squirrel, Computer Science 2026"},
+        { id: 4, content: 'PMReady helped me to find a career I was interested and passionate about pursuing within my major, while also giving me direct insight and practice into what being a PM entails.', author: "Naomy Bopela, Computer Science 2026"},
+        { id: 5, content: 'I appreciate how appealing this organization is to introducing product management in an easily understandable way. This org introduced me to a lot of people who inspired me to consider other educational fields.', author: "Cindy Frempong, Data Science 2026"},
+        { id: 6, content: 'It gave me a chance to explore a different career path within computer science that I had no prior knowledge of beforehand.', author: "Running Unicorn, Computer Science 2025"},
+        { id: 7, content: 'PMReady introduces me to a career position that I have never previously considered or known of. I used to think that working in the tech industry is overwhelming, and PMReady definitely helps provide more insight. PM has also become one of my top career choices.', author: "Lindsay Hu, Statistics & CS 2026"},
+        { id: 8, content: 'I learned about how product managers break down large problems into smaller achievable goals. PMReady showed me that there are other careers that relate to software development without needing to know all the intricacies of programming.', author: "Brianna Li, Math and Computer Science 2025"},
     ]
 
     return (
@@ -52,7 +53,7 @@ export default function Testimonial() {
                 onSwiper={(swiper) => console.log(swiper)}
             >
                 {testimonials.map((testimonial) => (
-                    <SwiperSlide className="p-5 lg:p-0" key={testimonial.id}><IndividualTestimonial  content={testimonial.content} className="" /></SwiperSlide>
+                    <SwiperSlide className="p-5 lg:p-0" key={testimonial.id}><IndividualTestimonial  content={testimonial.content} author={testimonial.author} className="" /></SwiperSlide>
                 ))}
             </Swiper>
         </div>

77d4a311a2366d7d3c413c4b541ad0bc3fdb7a50
samantha <samanthap8675@gmail.com>
2024-04-22 22:29:57 -0400

    fixed some minor adjustments on the mobile view

--- src/components/about/VolunteerBanner.jsx
+++ src/components/about/VolunteerBanner.jsx
@@ -6,7 +6,7 @@ export default function VolunteerBanner() {
                 PMReady is made <br className="block lg:hidden" /> <span className="text-[#FA9D74]">by PMs, </span> <span className="text-[#3BCABE]">for PMs</span>
             </h1>
             <div className="flex flex-col gap-5 items-center mt-[36px]">
-                <div className="flex flex-row gap-5 sm:hidden lg:block">
+                <div className=" flex-row gap-5 hidden lg:block">
                     <div className="inline-block justify-center text-center w-[211px]">
                         <img className="mx-auto block w-40 h-40 rounded-full" src="./images/macy.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
@@ -30,7 +30,7 @@ export default function VolunteerBanner() {
                         <p className="font-inter text-lg mt-2">Product Manager</p>
                     </div>
                 </div>
-                <div className="flex flex-row gap-5 sm:hidden lg:block">
+                <div className="flex-row gap-5 hidden lg:block">
                     {/*<div className="inline-block justify-center text-center w-[211px]">
                         <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/lenin.jpeg"></img>
                         <p className="font-lexend text-lg font-semibold mt-2" >Lenin Kennedy</p>
@@ -71,7 +71,7 @@ export default function VolunteerBanner() {
                 <div className="flex flex-row gap-2 sm:block lg:hidden">
                     <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
 
-                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full" src="./images/macy.jpeg"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/macy.jpeg"></img>
 
                         <p className="font-lexend text-lg font-semibold mt-2" >Macy So</p>
                         <p className="font-inter text-base min-[400px]:text-lg mt-2">Founder</p>
@@ -88,7 +88,7 @@ export default function VolunteerBanner() {
                 <div className="flex flex-row gap-2 sm:block lg:hidden">
                     <div className="inline-block justify-center text-center w-[150px] min-[400px]:w-[211px]">
 
-                        <img className="mx-auto block w-[107px] h-[107px] min-[400px]:w-40 min-[400px]:h-40 bg-[#D9D9D9] rounded-full" src="./images/catherine.jpg"></img>
+                        <img className="mx-auto block w-40 h-40 bg-[#D9D9D9] rounded-full" src="./images/catherine.jpg"></img>
 
                         <p className="font-lexend text-lg font-semibold mt-2" >Catherine Tai</p>
                         <p className="font-inter text-base min-[400px]:text-lg mt-2">Product Manager</p>

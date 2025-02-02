4250c0ec44d8ba5f39cb49779e82e51f3b7de8f8
samantha <samanthap8675@gmail.com>
2025-02-01 19:41:35 -0500

    fixed routing

--- src/components/P101/P101Banner.jsx
+++ src/components/P101/P101Banner.jsx
@@ -3,7 +3,7 @@ export default function Banner1() {
     <div className="flex flex-row justify-between pl-24 py-12 mb-4 bg-gradient-to-r from-green-100 from-12% via-orange-50 via-50% to-red-50">
       <div className="flex flex-col justify-between w-1/2">
         <h1 className="text-4xl font-bold h-1/5">P101 Program</h1>
-        <p className="h-3/5">
+        <p className="h-3/5 mt-4">
           Gain the skills you need to start your career in product management.
           Skills you need to thrive and develop in a product leader (Lorem
           Ipsum)
@@ -16,7 +16,7 @@ export default function Banner1() {
       </div>
 
       <div className="w-5/12">
-        <img src="https://picsum.photos/700/400" alt="" className="w-full" />
+        <img  alt="" className="w-full" />
       </div>
     </div>
   );
--- src/components/program/newProgramHero.jsx
+++ src/components/program/newProgramHero.jsx
@@ -2,26 +2,9 @@ import { MoveRight } from "lucide-react"
 
 export default function newHero() {
   return (
-    <div className="flex flex-col bg-[#FFF9F6] py-20">
-      <div className="flex flex-row justify-between pl-20">
-        <div className="relative w-1/2">
-          <h2>Learn product management while making a <span className="text-[#FA9D74] font-bold text-5xl">real difference</span></h2>
-          <p className="mt-[5%] text-xl w-3/4">A fully remote product fellowship program in the Fall where students in undergraduate programs can work with startups to gain real world experience.</p>
-          {/* some warning thing  */}
-          <div className="absolute flex justify-between w-1/2 mb-4 bottom-0">
-            <button className="flex flex-row px-8 py-2 border-[1px] text-[#FA9D74] font-bold border-[#FA9D74] rounded-full">Learn More<MoveRight className="ml-2" /></button>
-            <button className="px-8 py-2 text-white font-bold bg-[#0D1B2A] rounded-full ">Apply Now</button>
-          </div>
-        </div>
-        <img src="program/LearningHeader.png" alt="" className="w-[45%] rounded-tl-lg rounded-bl-lg" />
-      </div>
+    <div className="py-20 px-40 text-[48px] font-bold bg-[linear-gradient(to_right,rgba(6,183,167,0.2),rgba(140,198,63,0.2),rgba(243,198,116,0.2),rgba(252,193,168,0.2))]">
+  Programs
+</div>
 
-      <div className="flex flex-col w-full justify-center mt-5 ">
-        <h6 className="mr-[2rem] w-full text-center">Collaborate with product managers from</h6>
-        <div className="flex flex-row w-full justify-center mt-5">
-          <img src="microsoft.png" alt="" className="w-40" />
-        </div>
-      </div>
-    </div>
   )
 }
